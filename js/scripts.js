5e9170194283f9953b4c4c9946c9204e4a49d870
Seve Sheldon <sevesheldon@gmail.com>
2017-02-08 13:44:54 -0800

    add fullName method to contact prototype and update jquery to use it.

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,9 @@ function Contact(first, last) {
   this.lastName = last;
 }
 
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
 
 //user interface logic
 $(document).ready(function() {
@@ -16,7 +19,7 @@ $(document).ready(function() {
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName +"</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() +"</span></li>");
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
