0d0506610f18461407a6c2186af4615156bbd953
Seve Sheldon <sevesheldon@gmail.com>
2017-02-08 20:56:58 -0800

    Add the Address constructor and display the inputted values.

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,22 @@
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
+}
+
+function Address(street, city, state, zip) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.zip = zip;
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.street + " " + this.city + ", " + this.state + " " + this.zip;
 }
 
 //user interface logic
@@ -15,9 +27,17 @@ $(document).ready(function() {
     event.preventDefault();
 
     var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val()
-
+    var inputtedLastName = $("input#new-last-name").val();
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var inputtedZip = $(this).find("input.new-zip").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState, inputtedZip);
+      newContact.addresses.push(newAddress)
+    });
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() +"</span></li>");
 
@@ -26,11 +46,18 @@ $(document).ready(function() {
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+      });
     });
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
+    $("input").val("");
+    //if we want to clear ALL inputs just put ("input") like above.
+    //$("input#new-first-name").val("");
+    //$("input#new-last-name").val("");
 
+    //This makes the last contact (and all info) entered appear on the right
     //$('.contact').last().click();
 
   });
